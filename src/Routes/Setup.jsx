74bc390e5332b67183854f50d6545563033be0cb
Nofal <147974175+Nofal71@users.noreply.github.com>
2024-12-17 20:12:42 +0500

    setting up router

--- src/Routes/Setup.jsx
+++ src/Routes/Setup.jsx
@@ -1,28 +1,15 @@
-import { Button, Typography } from '@mui/material';
 import React from 'react';
-import useFeedBacks from '../redux/Providers/FeedBacksProviders';
+import { BrowserRouter as BrowserRoutes, Route, Routes } from 'react-router-dom';
+import Home from '../pages/Home/Home';
 
 const Setup = () => {
-  const { setAlert, setConfirm } = useFeedBacks();
-
-  const actions = [
-    { lable: 'Save', handler: () => setAlert('Saved', 'success'), sx: { color: 'green' } },
-    { lable: 'Cancle', handler: () => setAlert('Cancled', 'info'), sx: { color: 'red', backgroundColor: 'pink' } },
-  ]
-
   return (
-    <>
-      <h1>React Template</h1>
-      <Typography>MUI TEST</Typography>
-
-      <Button onClick={() => setAlert('Alert Opens Success', 'success')}>
-        Open Alert
-      </Button>
-      <Button onClick={() => setConfirm(true, 'title', 'aasjjjjjjjjjj sjajksnlaks         aklsnlkasndklas       saklxm', actions)}>
-        Open Alert Dialogue
-      </Button>
-
-    </>
+    <BrowserRoutes>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<div>Test Route</div>} />
+      </Routes>
+    </BrowserRoutes>
   );
 };
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
+import { Typography } from '@mui/material'
 import React from 'react'
 
 const Home = () => {
   return (
     <div>
-      
+      <Typography>Router Setup</Typography>
     </div>
   )
 }
