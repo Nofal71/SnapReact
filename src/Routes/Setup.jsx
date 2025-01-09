dae289529ae783e8e6ac78a1627c6b9f73d29fad
Nofal <147974175+Nofal71@users.noreply.github.com>
2025-01-09 21:06:59 +0500

    useSearch Hook, search Data / Filter Data Logic , Tutorial , Improve Code

--- src/Routes/Setup.jsx
+++ src/Routes/Setup.jsx
@@ -4,6 +4,7 @@ import Home from '../pages/Home/Home';
 import SignInPage from '../Authentications/LoginPage';
 import SignUpPage from '../Authentications/SignupPage';
 import NavBar from '../Components/page-components/NavBar';
+import SearchTurorial from '../pages/Home/SearchTutorial';
 
 const MainLayout = ({ children }) => (
   <>
@@ -23,10 +24,16 @@ const Setup = () => {
     <Router>
       <Routes>
         <Route path='/' element={
-            <MainLayout>
-              <Home />
-            </MainLayout>
-          }
+          <MainLayout>
+            <Home />
+          </MainLayout>
+        }
+        />
+        <Route path='/search' element={
+          <MainLayout>
+            <SearchTurorial />
+          </MainLayout>
+        }
         />
         <Route
           path='/login'
